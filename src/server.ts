3709acf030ab9b536f3ca646f352698f89e375eb
Pedro Carvalho <pedrofrancocarvalho@gmail.com>
2021-02-10 01:23:28 -0300

    day 2 finished

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,22 @@
 import express from 'express';
+import path from 'path';
+import cors from 'cors';
+
+import 'express-async-errors';
+
 import './database/connection';
 
+import routes from './routes';
+import errorsHandler from './errors/handler';
+
 const app = express();
+
+app.use(cors());
 app.use(express.json());
+app.use(routes);
+app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use(errorsHandler);
 
-app.get('/orphanages', (req, res) => {
-  return res.json({ message: 'Hellow World' });
+app.listen(3333, () => {
+  console.log('Server running at 3333...');
 });
-
-app.listen(3333);
